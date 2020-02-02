22876d077f26d9648bde0260165af3302b0c0fbc
Elton Cardoso do Nascimento <eltsu3110@hotmail.com>
2020-02-02 10:09:30 -0300

    Tom e Patinador Ajustado
    Ajustado temporização do Genius e dinâmica do patinador

--- Projeto1/Script/Patinador.gd
+++ Projeto1/Script/Patinador.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 var velocidade = Vector2(0,0)
-var acel = 50
-var velocidade_max = 200
-var atrito = 30
+var acel = 100
+var velocidade_max = 500
+var atrito = 40
+
+var bateu = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,11 +20,35 @@ func _process(delta):
 			velocidade.x += acel*delta
 		elif(!Input.is_action_pressed("Direita") and Input.is_action_pressed("Esquerda")):	
 			velocidade.x -= acel*delta
+		else:
+			#Atrito X
+			if(velocidade.x > 0):
+				velocidade.x -= atrito*delta
+				
+				if(velocidade.x<0):
+					velocidade.x = 0
+			elif(velocidade.x < 0):
+				velocidade.x += atrito*delta
+				
+				if(velocidade.x>0):
+					velocidade.x = 0
 			
 		if(Input.is_action_pressed("Cima") and !Input.is_action_pressed("Baixo")):
 			velocidade.y -= acel*delta
 		elif(!Input.is_action_pressed("Cima") and Input.is_action_pressed("Baixo")):	
 			velocidade.y += acel*delta
+		else:
+			#Atrito Y	
+			if(velocidade.y > 0):
+				velocidade.y -= atrito*delta
+				
+				if(velocidade.y<0):
+					velocidade.y = 0
+			elif(velocidade.y < 0):
+				velocidade.y += atrito*delta
+				
+				if(velocidade.y>0):
+					velocidade.y = 0
 		
 	if(velocidade.x>velocidade_max):
 		velocidade.x = velocidade_max
@@ -32,30 +58,25 @@ func _process(delta):
 	
 	
 	#Computa o atrito
-	#Atrito X
-	if(velocidade.x > 0):
-		velocidade.x -= atrito*delta
-		
-		if(velocidade.x<0):
-			velocidade.x = 0
-	elif(velocidade.x < 0):
-		velocidade.x += atrito*delta
-		
-		if(velocidade.x>0):
-			velocidade.x = 0
 	
-	#Atrito Y	
-	if(velocidade.y > 0):
-		velocidade.y -= atrito*delta
-		
-		if(velocidade.y<0):
-			velocidade.y = 0
-	elif(velocidade.y < 0):
-		velocidade.y += atrito*delta
-		
-		if(velocidade.y>0):
-			velocidade.y = 0
+	move_and_slide(velocidade, Vector2(0,0))
 	
-	move_and_slide_with_snap(velocidade, Vector2(0,5),Vector2(0, -1))
-		
+	if(is_on_wall() == true and bateu == false):
+		bateu = true
+		velocidade.x /= 10
+		velocidade.y /= 10
+	
+	if(is_on_wall() == false):
+		bateu = false	
+	
 	pass
+
+
+func _on_Area2D_body_entered(body):
+	atrito = 0
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	atrito = 40
+	pass # Replace with function body.
